cb93baa4e9a6716ee1db923dfc4978b92fe6f486
Luca Ban <mesqueeb@users.noreply.github.com>
2026-09-26 10:54:57 +1200

    chore: rebuild dist

--- dist/libNode/decodeIdToken.js
+++ dist/libNode/decodeIdToken.js
@@ -1,9 +1,9 @@
 import jwt from 'jsonwebtoken';
 const { decode } = jwt;
 /**
- * (Node only) Returns a decoded LINE id token. Uses the nodeJS 'jsonwebtoken' dependency.
- * This id Token should be validated!
- * LINE documentation: https://developers.line.biz/en/docs/line-login/integrate-line-login/#decode-and-validate-id-token
+ * (Node only) Returns a decoded LINE id token. Uses the nodeJS 'jsonwebtoken' dependency. This id
+ * Token should be validated! LINE documentation:
+ * https://developers.line.biz/en/docs/line-login/integrate-line-login/#decode-and-validate-id-token
  */
 export function decodeIdToken(idToken) {
     // get the decoded payload ignoring signature, no secretOrPrivateKey needed
--- dist/libNode/issueAccessToken.js
+++ dist/libNode/issueAccessToken.js
@@ -1,10 +1,11 @@
 import got from 'got';
 const post = got.post;
 /**
- * (Node only) Makes a POST request to retrieve an access token from LINE. This uses GOT as a dependency to make the request.
- * Can throw errors.
+ * (Node only) Makes a POST request to retrieve an access token from LINE. This uses GOT as a
+ * dependency to make the request. Can throw errors.
  *
- * LINE documentation: https://developers.line.biz/en/docs/line-login/integrate-line-login/#get-access-token
+ * LINE documentation:
+ * https://developers.line.biz/en/docs/line-login/integrate-line-login/#get-access-token
  *
  * @param {IssueAccessTokenParams} params
  * @returns {Promise<IssueAccessTokenResponse>}
@@ -15,9 +16,10 @@ export async function issueAccessToken(params) {
         throw new Error('Missing information in params');
     }
     /**
-     * To get an access token, make an HTTP POST request with the authorization code. Once you have an access token, you can use it to make API calls. The access token is issued at the following endpoint.
-     * The information in the request body should be:
-     * Content-Type: application/x-www-form-urlencoded
+     * To get an access token, make an HTTP POST request with the authorization code. Once you have an
+     * access token, you can use it to make API calls. The access token is issued at the following
+     * endpoint. The information in the request body should be: Content-Type:
+     * application/x-www-form-urlencoded
      */
     const form = { grant_type, code, redirect_uri, client_id, client_secret };
     const response = await post('https://api.line.me/oauth2/v2.1/token', {
